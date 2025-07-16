24f35087afe8c47b4e2f04bf85b3f8c22db5628d
aled <alededwards32@gmail.com>
2025-07-16 23:22:59 +0100

    Chip8Emulator: Initial code setup
    setups inital code and paths

---
+++ src/main.cpp
@@ -0,0 +1,35 @@
+#define SDL_MAIN_USE_CALLBACKS 1
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+#include <iostream>
+
+static SDL_Window* window = nullptr;
+static SDL_Renderer* renderer = nullptr;
+
+SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[])
+{
+    if(!SDL_CreateWindowAndRenderer("Chip8Emulator",400,400,SDL_WINDOW_FULLSCREEN, &window, &renderer))
+    {
+        SDL_Log("Couldn't create window and renderer: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
+{
+    if (event->type == SDL_EVENT_KEY_DOWN ||
+        event->type == SDL_EVENT_QUIT) {
+        return SDL_APP_SUCCESS; 
+    }
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppIterate(void *appstate)
+{
+    return SDL_APP_CONTINUE;
+}
+
+void SDL_AppQuit(void *appstate, SDL_AppResult result)
+{
+}
